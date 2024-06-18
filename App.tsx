1ddf376d1b159ed3bd091bcf0833df181f77dfd0
thanhnam1997-97 <izui1997vngmail.com>
2024-06-18 09:52:12 +0700

    add 18/06

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import AuthNavigator from './src/navigatiors/AuthNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {SplashScreen} from './src/screens';
 import {StatusBar} from 'react-native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigatiors/MainNavigator';
 
 const App = () => {
   const [isShowSplash, setIsShowSplash] = useState(true);
+  const [accessToken, setAccessToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('accessToken');
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
@@ -13,6 +18,15 @@ const App = () => {
 
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await getItem();
+    token && setAccessToken(token);
+  };
 
   return (
     <>
@@ -25,7 +39,7 @@ const App = () => {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       )}
     </>
